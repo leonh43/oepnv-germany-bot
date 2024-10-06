675127d1aab8b796033e32ea97080e55665cf3de
Leon <118343218+leonh43@users.noreply.github.com>
2024-10-06 20:38:08 +0200

    Added new file (wont work)

---
+++ index.js
@@ -0,0 +1,18 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
+client.once('ready', () => {
+    console.log('Bot is online!');
+});
+
+client.on('messageCreate', (message) => {
+    if (message.content === '!ping') {
+        message.channel.send('Pong!');
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
